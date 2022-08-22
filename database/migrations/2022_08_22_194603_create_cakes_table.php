b7df6bb8ed342669d78f3fa61f01d5b1377840e8
Wanderley Ferreira de Albuquerque <wanderleyfa@gmail.com>
2022-08-22 19:37:46 -0300

    feat: cake database definition

---
+++ database/migrations/2022_08_22_194603_create_cakes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCakesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cakes', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->integer('weight');
+            $table->double('value', 9, 2);
+            $table->integer('inventory');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cakes');
+    }
+}
